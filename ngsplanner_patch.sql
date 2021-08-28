bbbce91146ace85c3df6f6e35a42615d7988ef7a
sigonasr2 <sigonasr2@gmail.com>
2021-08-28 05:42:14 -0500

    Remove unused files

--- ngsplanner_patch.sql
+++
@@ -1,5 +0,0 @@
-/*27 Aug Database Patch*/
-CREATE TABLE "element" ("id" SERIAL UNIQUE PRIMARY KEY,"name" text);
-alter table augment add column element_id int;
-alter table augment add column icon text;
-alter table augment_type drop column icon;
